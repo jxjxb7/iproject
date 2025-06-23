dc7e4a32fe31c9b789787808ec91a538f1532a4b
jxjxb7 <153766702+jxjxb7@users.noreply.github.com>
2025-06-24 00:32:18 +0300

    Updated UserManagement.tsx

--- src/components/UserManagement.tsx
+++ src/components/UserManagement.tsx
@@ -305,13 +305,13 @@ export function UserManagement() {
           </span>
         </div>
         
-
+      
           {currentUser?.role === 'admin' && (
             <button
               onClick={() => setShowAddUser(true)}
               className="flex items-center space-x-2 text-gray-800 px-4 py-2 rounded-xl font-medium uppercase"
               style={{ backgroundColor: '#CFE8FF' }}
-            >
+            
               <UserPlus className="w-5 h-5" />
               <span>ДОБАВИТЬ ПОЛЬЗОВАТЕЛЯ</span>
             </button>
